b06bdc71c9d0347be1bdad24ad24c017b63f4fa9
Gopal S Akshintala <gakshintala@salesforce.com>
2020-03-14 18:07:17 +0530

    Gradle deps upgrade

--- kofu-reactive-validation/build.gradle.kts
+++ kofu-reactive-validation/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.springframework.boot") version "2.3.0.M1"
 }
 
@@ -14,7 +14,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RELEASE")
-    implementation("io.r2dbc:r2dbc-h2:0.8.2.RELEASE")
+    implementation("io.r2dbc:r2dbc-h2")
 
     implementation("io.arrow-kt:arrow-core:$arrowSnapshotVersion")
     implementation("io.arrow-kt:arrow-fx:$arrowSnapshotVersion")
@@ -27,7 +27,7 @@ dependencies {
 
 dependencyManagement {
     imports {
-        mavenBom("io.r2dbc:r2dbc-bom:Arabba-RELEASE")
+        mavenBom("io.r2dbc:r2dbc-bom:Arabba-SR2")
     }
 }
 
--- validation-fx/build.gradle.kts
+++ validation-fx/build.gradle.kts
@@ -2,13 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
 }
 
 val arrowSnapshotVersion = "0.10.5-SNAPSHOT"
 val arrowVersion = "0.10.4"
 
 dependencies {
-    implementation("io.r2dbc:r2dbc-h2:0.8.2.RELEASE")
+    implementation("io.r2dbc:r2dbc-h2")
     implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RC1")
 
     implementation("io.arrow-kt:arrow-core:$arrowSnapshotVersion")
@@ -16,10 +17,18 @@ dependencies {
     implementation("io.arrow-kt:arrow-fx-reactor:$arrowSnapshotVersion")
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("io.r2dbc:r2dbc-bom:Arabba-SR2")
+    }
+}
+
 repositories {
     mavenLocal()
     mavenCentral()
     gradlePluginPortal()
+    maven("https://repo.spring.io/milestone")
+    maven("https://repo.spring.io/snapshot")
     maven(url = "https://dl.bintray.com/arrow-kt/arrow-kt/")
     maven(url = "https://oss.jfrog.org/artifactory/oss-snapshot-local/")
 }
