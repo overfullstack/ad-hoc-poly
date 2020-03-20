384794582b9ffdeff99c94a083cd7c5d8fa05a15
Gopal S Akshintala <gakshintala@salesforce.com>
2020-03-20 19:32:52 +0530

    Upgrading Spring-boot

--- kofu-reactive-validation/build.gradle.kts
+++ kofu-reactive-validation/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.70"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id("org.springframework.boot") version "2.3.0.M1"
+    id("org.springframework.boot") version "2.3.0.M3"
 }
 
 val arrowSnapshotVersion = "0.10.5-SNAPSHOT"
