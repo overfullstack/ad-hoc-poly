3c814029836901852e4a828ed985a4ae6093ffd8
Gopal S Akshintala <gakshintala@salesforce.com>
2020-04-05 09:24:36 +0530

    Upgrade - spring-boot, spring-data-r2dbc

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.71" apply false
     id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
-    id("org.springframework.boot") version "2.3.0.M1" apply false
+    id("org.springframework.boot") version "2.3.0.M4" apply false
 }
 
 subprojects {
--- kofu-reactive-validation/build.gradle.kts
+++ kofu-reactive-validation/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RELEASE")
+    implementation("org.springframework.data:spring-data-r2dbc:1.1.0.RC1")
     implementation("io.r2dbc:r2dbc-h2")
 }
